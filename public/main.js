ba13790132fa702e7026da6383439a2849b8630e
Benjamin JEHL <benjaminjehl@Benjamins-MBP.fios-router.home>
2018-09-18 21:00:45 -0400

    page resetting

--- public/main.js
+++ public/main.js
@@ -4,13 +4,25 @@
 // click button and checks input (city) === done
 // click button and fetch data for weather === done
 // return information about weather temp === done
-// create li element
-// append child ul element to add info to the DOM
+// create li element === done
+// append child ul element to add info to the DOM === done
 // repeat steps for more information
 // get more information including current conditions
 // put on the DOM
+// const resetTheCity = () => {
+//   let topList = document.querySelector('.reset')
+//   let listItem = document.querySelector('.reset-item')
+//   let remove = () => {
+//     topList.removeChild(listItem)
+//   }
+//   remove()
+// }
 
 const goForWeather = () => {
+  let mainContainer = document.querySelector('ul.reset')
+  while (mainContainer.firstChild) {
+    mainContainer.removeChild(mainContainer.firstChild)
+  }
   let searchInput = document.querySelector('.city-input')
   let citySearch = searchInput.value
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&units=imperial&appid=ab775780f3f23d518c06143e1db7c763`)
@@ -38,10 +50,9 @@ const goForWeather = () => {
 }
 const main = () => {
 
-
-
   document.querySelector('.go-button').addEventListener('click', goForWeather)
 
 }
 
 document.addEventListener('DOMContentLoaded', main)
+
