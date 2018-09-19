d427704664d46ddb80c03bdb4d3541cfefdc33be
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-09-19 14:49:00 -0400

    class works for name and temp

--- public/main.js
+++ public/main.js
@@ -1,4 +1,15 @@
+class WeatherList {
+  constructor(weatherSelector) {
+    this.mainWeather = document.querySelector(weatherSelector)
+  }
 
+  addWeather(message) {
+    const weatherInfo = document.createElement('li')
+    weatherInfo.textContent = message
+    this.mainWeather.appendChild(weatherInfo)
+  }
+
+}
 
 const goForWeather = () => {
   let mainContainer = document.querySelector('ul.reset')
@@ -22,28 +33,10 @@ const goForWeather = () => {
       return response.json()
     })
     .then(weatherAttributes => {
-      let weatherOnPage = document.querySelector('.weather-info')
+      const weatherList = new WeatherList('.weather-info')
+      weatherList.addWeather(weatherAttributes.name)
+      weatherList.addWeather(weatherAttributes.main.temp)
 
-      // If there is no such city
-      if (weatherAttributes.cod === '404') {
-        let error = document.createElement('li')
-        error.textContent = 'Not a place. Batman laughs at you!'
-        weatherOnPage.appendChild(error)
-      } else {
-        let weatherLiName = document.createElement('li')
-        weatherLiName.textContent = weatherAttributes.name
-        weatherOnPage.appendChild(weatherLiName)
-
-        let weatherLiTemp = document.createElement('li')
-        weatherLiTemp.textContent = `${weatherAttributes.main.temp} Fahrenheit`
-        weatherOnPage.appendChild(weatherLiTemp)
-
-        weatherAttributes.weather.forEach(weatherCondition => {
-          let weatherLiCondition = document.createElement('li')
-          weatherLiCondition.textContent = weatherCondition.main
-          weatherOnPage.appendChild(weatherLiCondition)
-        })
-      }
     }
     )
 }
@@ -53,3 +46,26 @@ const main = () => {
 }
 
 document.addEventListener('DOMContentLoaded', main)
+
+      //let weatherOnPage = document.querySelector('.weather-info')
+//
+      //// If there is no such city
+      //if (weatherAttributes.cod === '404') {
+      //  let error = document.createElement('li')
+      //  error.textContent = 'Not a place. Batman laughs at you!'
+      //  weatherOnPage.appendChild(error)
+      //} else {
+      //  let weatherLiName = document.createElement('li')
+      //  weatherLiName.textContent = weatherAttributes.name
+      //  weatherOnPage.appendChild(weatherLiName)
+//
+      //  let weatherLiTemp = document.createElement('li')
+      //  weatherLiTemp.textContent = `${weatherAttributes.main.temp} Fahrenheit`
+      //  weatherOnPage.appendChild(weatherLiTemp)
+//
+      //  weatherAttributes.weather.forEach(weatherCondition => {
+      //    let weatherLiCondition = document.createElement('li')
+      //    weatherLiCondition.textContent = weatherCondition.main
+      //    weatherOnPage.appendChild(weatherLiCondition)
+      //  })
+      //}
